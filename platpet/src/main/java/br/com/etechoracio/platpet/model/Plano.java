67d56b3137a0f9a07881684ce84ed9edf3716ae5
Anderson Pedrassi <anderson_pedrassi@hotmail.com>
2019-11-01 21:59:47 -0300

    Ajuste da API

---
+++ platpet/src/main/java/br/com/etechoracio/platpet/model/Plano.java
@@ -0,0 +1,37 @@
+package br.com.etechoracio.platpet.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Table(name="PLANO")
+@Entity
+@Setter
+@Getter
+public class Plano {
+	
+	@Column(name="IDPLANO")
+	@GeneratedValue
+	@Id
+	private int id;
+	
+	@Column(name="TIPOPLANO")
+	private int tipoplano;
+	
+	@Column(name="DESCPLANO")
+	private String descplano;
+	
+	@Column(name="DURAPLANO")
+	private int duraplano;
+	
+	@Column(name="VALORPLANO")
+	private long valorplano;
+	
+
+}
