e253d6cbb8063a31def85ecb1b9fe2b85d66788d
Aluno ETEC HAS <anderson_pedrassi@hotmail.com>
2019-11-08 22:20:27 -0300

    Put e Delete da controller

--- platpet/src/main/java/br/com/etechoracio/platpet/controller/PlanoController.java
+++ platpet/src/main/java/br/com/etechoracio/platpet/controller/PlanoController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -39,7 +41,7 @@ public class PlanoController {
 				.orElse(ResponseEntity.notFound().build());
 	}
 	
-	@GetMapping(value="/{id}")
+	@PutMapping(value="/{id}")
 	public ResponseEntity<Plano> update(@PathVariable("id") Integer id,
 										@RequestBody Plano plan){
 	return dao.findById(id)
@@ -51,5 +53,15 @@ public class PlanoController {
 				Plano updated = dao.save(record);
 				return ResponseEntity.ok().body(updated);
 				}).orElse(ResponseEntity.notFound().build());
+	}
+	
+	@DeleteMapping(path = {"/{id}"})
+	public ResponseEntity<?> delete(@PathVariable("id") Integer id){
+		return dao.findById(id)
+				.map(record -> {
+					dao.deleteById(id);
+					return ResponseEntity.ok().build();
+				}).orElse(ResponseEntity.notFound().build());
+
 	}
 }
