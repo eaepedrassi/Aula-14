d7627b3045c65ae95f0fe89e1159c64500054f42
Aluno ETEC HAS <anderson_pedrassi@hotmail.com>
2019-11-08 21:50:40 -0300

    Ajuste da controller

--- platpet/src/main/java/br/com/etechoracio/platpet/controller/PlanoController.java
+++ platpet/src/main/java/br/com/etechoracio/platpet/controller/PlanoController.java
@@ -1,9 +1,12 @@
 package br.com.etechoracio.platpet.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,4 +22,34 @@ public class PlanoController {
 	@Autowired
 	private PlanoDAO dao;
 	
+	@PostMapping
+	public void insert(@RequestBody Plano plan) {
+		dao.save(plan);
+	}
+	
+	@GetMapping
+	public List<Plano> findAll(){
+		return dao.findAll();
+	}
+	
+	@GetMapping(path = {"/{id}"})
+	public ResponseEntity<Plano> findById(@PathVariable Integer id){
+		return dao.findById(id)
+				.map(record -> ResponseEntity.ok().body(record))
+				.orElse(ResponseEntity.notFound().build());
+	}
+	
+	@GetMapping(value="/{id}")
+	public ResponseEntity<Plano> update(@PathVariable("id") Integer id,
+										@RequestBody Plano plan){
+	return dao.findById(id)
+			.map(record -> {
+				record.setTipoplano(plan.getTipoplano());
+				record.setDescplano(plan.getDescplano());
+				record.setDuraplano(plan.getDuraplano());
+				record.setValorplano(plan.getValorplano());
+				Plano updated = dao.save(record);
+				return ResponseEntity.ok().body(updated);
+				}).orElse(ResponseEntity.notFound().build());
+	}
 }
